3bae3eedb329f58f316f9044bb4000ce23a1b552
reginaldbdanso <reginaldbdanso@gmail.com>
2024-02-01 18:37:27 +0000

    Updated the routes to invoke the various controller functions depending on the request path.

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -1,30 +1,26 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
+const {
+    createWorkout,
+    getWorkouts,
+    getWorkout,
+    deleteWorkout,
+    updateWorkout
+} = require('../controllers/workoutController');
 
 // GET all workouts
-router.get('/', (req, res) => {
-    res.json({message: 'Get all workouts'})
-
-})
+router.get('/', getWorkouts)
 
 //GET a single workout
-router.get('/:id', (req, res) => {
-    res.json({message: 'Get a single workout'})
-})
+router.get('/:id', getWorkout)
 
 //POST a new workout
-router.post('/', (req, res) => {
-    res.json({message: 'Post a new workout'})
-})
+router.post('/', createWorkout)
 
-//DELETE a wprkout
-router.delete('/:id', (req, res) => {
-    res.json({message: 'Delete a workout'})
-})
+//DELETE a workout
+router.delete('/:id', deleteWorkout)
 
 // UPDATE a workout
-router.patch('/:id', (req, res) => {
-    res.json({message: 'Update a workout'})
-})
+router.patch('/:id', updateWorkout)
 
 module.exports = router
