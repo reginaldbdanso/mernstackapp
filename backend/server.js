b0847a805b71b0261e4ddd1e22b06c30de41db3e
reginaldbdanso <reginaldbdanso@gmail.com>
2024-02-02 15:21:04 +0000

    Added cors

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const router = require('./routes/router');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 
 // express app
@@ -10,6 +11,7 @@ const app = express()
 
 // middleware
 app.use(express.json());
+app.use(cors());
 app.use((req, res, next) => {
     console.log(req.path, req.method);
     next();
